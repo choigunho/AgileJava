d47d096ca3b770663c4f3a6aa94ce21d906930d0
choigunho <choiguenho@gmail.com>
2014-09-08 20:51:30 +0900

    p383

--- AgileJava/src/sis/report/RosterReporter.java
+++ AgileJava/src/sis/report/RosterReporter.java
@@ -1,48 +1,83 @@
 package sis.report;
 
-import sis.studentinfo.CourseSession;
-import sis.studentinfo.Session;
-import sis.studentinfo.Student;
+import java.util.*;
+import sis.studentinfo.*;
 import static sis.report.ReporterConstant.NEWLINE;
-//import java.util.*;
+import java.io.*;
 
 class RosterReporter {
-//	static final String NEWLINE = System.getProperty("line.separator");
-	static final String ROSTER_REPORT_HEADER = 
-			"Student" + NEWLINE +
-			"-" + NEWLINE;
-	static final String ROSTER_REPORT_FOOTER = 
-			NEWLINE + "# students = ";
+	static final String ROSTER_REPORT_HEADER = "Student%n-%n";
+	static final String ROSTER_REPORT_FOOTER = "%n# students = %d%n";
+	
+//	static final String ROSTER_REPORT_HEADER = 
+//			"Student" + NEWLINE +
+//			"-" + NEWLINE;
+//	static final String ROSTER_REPORT_FOOTER = 
+//			NEWLINE + "# students = ";
 	
 	private Session session;
+	private Writer writer;
 	
 	RosterReporter(Session session) {
 		this.session = session;
 	}
 	
-	String getReport() {
-		StringBuilder buffer = new StringBuilder();
-		writeHeader(buffer);
-		writeBody(buffer);
-		writeFooter(buffer);
-		
-		return buffer.toString();
+	void writeReport(Writer writer) throws IOException {
+		this.writer = writer;
+		writeHeader();
+		writeBody();
+		writeFooter();
 	}
 	
-	void writeHeader(StringBuilder buffer) {
-		buffer.append(ROSTER_REPORT_HEADER);
-	}
-	
-	void writeBody(StringBuilder buffer) {
-		for(Student student : session.getAllStudents()) {
-			buffer.append(student.getName());
-			buffer.append(NEWLINE);
+	void writeReport(String filename) throws IOException {
+		Writer bufferedWriter = new BufferedWriter(new FileWriter(filename));
+		try {
+			writeReport(bufferedWriter);
+		}
+		finally {
+			bufferedWriter.close();
 		}
 	}
 	
-	void writeFooter(StringBuilder buffer) {
-		buffer.append(
-				ROSTER_REPORT_FOOTER + session.getAllStudents().size() + 
-				NEWLINE);
+	private void writeHeader() throws IOException {
+//		writer.write(ROSTER_REPORT_HEADER);
+		writer.write(String.format(ROSTER_REPORT_HEADER));
 	}
+	
+	private void writeBody() throws IOException {
+		for(Student student: session.getAllStudents())
+//			writer.write(student.getName() + NEWLINE);
+			writer.write(String.format(student.getName() + "%n"));
+	}
+	
+	private void writeFooter() throws IOException {
+		writer.write(
+				String.format(ROSTER_REPORT_FOOTER, session.getAllStudents().size()));
+	}
+	
+//	String getReport() {
+//		StringBuilder buffer = new StringBuilder();
+//		writeHeader(buffer);
+//		writeBody(buffer);
+//		writeFooter(buffer);
+//		
+//		return buffer.toString();
+//	}
+	
+//	private void writeHeader(StringBuilder buffer) {
+//		buffer.append(ROSTER_REPORT_HEADER);
+//	}
+	
+//	private void writeBody(StringBuilder buffer) {
+//		for(Student student : session.getAllStudents()) {
+//			buffer.append(student.getName());
+//			buffer.append(NEWLINE);
+//		}
+//	}
+	
+//	private void writeFooter(StringBuilder buffer) {
+//		buffer.append(
+//				ROSTER_REPORT_FOOTER + session.getAllStudents().size() + 
+//				NEWLINE);
+//	}
 }
--- AgileJava/src/sis/report/RosterReporterTest.java
+++ AgileJava/src/sis/report/RosterReporterTest.java
@@ -1,25 +1,71 @@
 package sis.report;
 
-import sis.studentinfo.*;
 import junit.framework.TestCase;
-//import java.util.*;
+import sis.studentinfo.*;
 import static sis.report.ReporterConstant.NEWLINE;
 
+import java.io.*;
+
 public class RosterReporterTest extends TestCase {
-	public void testRosterReport() {
-//		CourseSession session = CourseSession.create("ENGL", "101", DateUtil.createDate(2003, 1, 6));
-		Session session = CourseSession.create(new Course("ENGL", "101"), DateUtil.createDate(2003, 1, 6));
+	private Session session;
+	
+	protected void setUp() {
+		session = CourseSession.create(
+				new Course("ENGL", "101"),
+				DateUtil.createDate(2003, 1, 6));
 		
 		session.enroll(new Student("A"));
 		session.enroll(new Student("B"));
+	}
+	
+	public void testRosterReport() throws IOException {
+		Writer writer = new StringWriter();
+		new RosterReporter(session).writeReport(writer);
+		String rosterReport = writer.toString();
 		
-		String rosterReport = new RosterReporter(session).getReport();
 		System.out.println(rosterReport);
+		
 		assertEquals(
-				RosterReporter.ROSTER_REPORT_HEADER +
-				"A" + NEWLINE +
-				"B" + NEWLINE +
-				RosterReporter.ROSTER_REPORT_FOOTER + "2" +
-				NEWLINE, rosterReport);
+				String.format(RosterReporter.ROSTER_REPORT_HEADER +
+						"A%n" +
+						"B%n" +
+						RosterReporter.ROSTER_REPORT_FOOTER, 2),
+				rosterReport);
+		
+//		assertEquals(
+//				RosterReporter.ROSTER_REPORT_HEADER +
+//				"A" + NEWLINE +
+//				"B" + NEWLINE +
+//				RosterReporter.ROSTER_REPORT_FOOTER + "2" +
+//				NEWLINE, rosterReport);
+		
+	}
+	
+	public void testFiledReport() throws IOException {
+		final String filename = "testFiledReport.txt";
+		try {
+			delete(filename);
+			
+			new RosterReporter(session).writeReport(filename);
+			
+			StringBuffer buffer = new StringBuffer();
+			String line;
+			
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			while((line = reader.readLine()) != null)
+				buffer.append(String.format(line + "%n"));
+			reader.close();
+			
+//		assertReportContents(buffer.toString());
+			
+		}
+		finally {
+			delete(filename);
+		}
+	}
+	private void delete(String filename) {
+		File file = new File(filename);
+		if(file.exists())
+			assertTrue("unable to delete " + filename, file.delete());
 	}
 }
